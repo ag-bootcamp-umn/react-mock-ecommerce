4927f6c318b6057cec6af74003d075c2fe79d3d9
alejandro <alexgeorgegeer@gmail.com>
2024-02-26 10:56:40 -0600

    added newProduct form

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,36 @@
 import { useEffect, useState } from "react";
 import Cart from "./components/Cart";
 import Products from "./components/Products";
-import widgets from "./widgets.json"
+import widgets from "./widgets.json";
 import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import AddProduct from "./components/AddProduct";
 
 function App() {
+  const defaultForm = { name: "", price: "", quantity: "" };
   const [products, setProduct] = useState(widgets);
   const [cart, setCart] = useState([]);
+  const [newProduct, setNewProduct] = useState(defaultForm);
 
   function checkForSavedData() {
-    const productData = localStorage.getItem('productData');
+    const productData = localStorage.getItem("productData");
     if (productData) {
-        return JSON.parse(productData)
-    } 
-};
+      return JSON.parse(productData);
+    }
+  }
 
-  useEffect( () => {
-    console.log('p' , products)
-  }, [products]);
+  // useEffect(() => {
+  //   console.log("p", products);
+  // }, [products]);
 
-  useEffect(() => {
-    const productData = checkForSavedData();
-    if (productData) setProduct(productData);
-  }, []);
+  // useEffect(() => {
+  //   const productData = checkForSavedData();
+  //   if (productData) setProduct(productData);
+  // }, []);
+
+  // useEffect( () => {
+  //   console.log('new product', newProduct)
+  // }, [newProduct]);
 
   return (
     <>
@@ -32,9 +39,23 @@ function App() {
           <div className="col-6">
             <Products products={products} setCart={setCart} cart={cart} />
           </div>
+          <div className="col-6">
+            <AddProduct
+              setNewProduct={setNewProduct}
+              newProduct={newProduct}
+              products={products}
+              setProduct={setProduct}
+              defaultForm={defaultForm}
+            ></AddProduct>
+          </div>
 
           <div className="col-6">
-            <Cart cart={cart} setCart={setCart} products={products} setProduct={setProduct}/>
+            <Cart
+              cart={cart}
+              setCart={setCart}
+              products={products}
+              setProduct={setProduct}
+            />
           </div>
         </div>
       </div>
---
+++ src/components/AddProduct.jsx
@@ -0,0 +1,62 @@
+export default function AddProduct(props) {
+
+  function handleInputChange(e) {
+    props.setNewProduct({...props.newProduct, [e.target.name]: e.target.value})
+  }
+
+  function addNewProduct(e) {
+    e.preventDefault();
+    console.log('newProduct', props.newProduct)
+    props.setProduct([...props.products, props.newProduct]);
+    props.setNewProduct(props.defaultForm);
+  }
+
+  return (
+    <>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="name" className="form-label">
+            Product Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            className="form-control"
+            value={props.newProduct.name}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="price" className="form-label">
+            Price
+          </label>
+          <input
+            type="text"
+            name="price"
+            id="price"
+            className="form-control"
+            value={props.newProduct.price}
+            onChange={handleInputChange}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="quantity" className="form-label">
+            Quantity
+          </label>
+          <input
+            type="text"
+            name="quantity"
+            id="quantity"
+            className="form-control"
+            value={props.newProduct.quantity}
+            onChange={handleInputChange}
+          />
+        </div>
+
+
+        <button className="btn btn-primary" onClick={addNewProduct}>Save Note</button>
+      </form>
+    </>
+  )
+}
